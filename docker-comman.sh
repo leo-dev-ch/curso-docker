3827d7c4f0053dd54a41b37fd64f279dfca7dda5
leo-pc <leo.garay9@gmail.com>
2020-05-23 20:01:15 -0300

    se agregaron archivo docker compose y docker command

---
+++ docker-comman.sh
@@ -0,0 +1,16 @@
+## Comandos docker
+
+docker run -d -v www:/var/www/html --name=wp --link=db_wp \
+			--env WORDPRESS_DB_HOST=db_wp:3306   \
+			--env WORDPRESS_DB_USER=wordpress      \
+			--env WORDPRESS_DB_PASSWORD=wordpress      \
+			--env WORDPRESS_DB_NAME=wordpress   \
+			-p 8009:80 wordpress:latest
+
+docker run -d -v db_data:/var/lib/mysql --name=db_wp  \
+			--env MYSQL_DATABASE=wordpress  \
+			--env  MYSQL_ROOT_PASSWORD=somewordpress  \
+			--env  MYSQL_DATABASE=wordpress   \
+			--env  MYSQL_USER=wordpress  \
+			--env  MYSQL_PASSWORD=wordpress   \
+			-p 3307:3306 mysql:5.7
